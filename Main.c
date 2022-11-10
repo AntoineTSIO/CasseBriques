2b460ad704f7f76ae68fbf31ff4bfe744e086c52
AntoineTSIO <antoine.touzart.sio@gmail.com>
2022-11-10 21:46:01 +0100

    feat: start SDL implementation

--- Main.c
+++ Main.c
@@ -5,11 +5,29 @@
 
 int main(int argc, char **argv) {
 
+    SDL_Window *sdlWindow = NULL;
+
     //SDL version
     SDL_version nb;
     SDL_VERSION(&nb);
     printf("SDL version %d.%d.%d !\n", nb.major, nb.minor, nb.patch);
     //SDL version
+
+    //SDL Init Verification
+    if(SDL_Init(SDL_INIT_VIDEO) != 0)
+    {
+        SDL_Log("Unable to initialize SDL: %s\n", SDL_GetError());
+        exit(EXIT_FAILURE);
+    }
+
+    //SDL execution...
+    sdlWindow = SDL_CreateWindow("Casse Briques en C",SDL_WINDOWPOS_CENTERED,SDL_WINDOWPOS_CENTERED,500,800,SDL_WINDOW_RESIZABLE); // Window title, x pos, y pos, width, height, flags
+    if(sdlWindow == NULL)
+    {
+        SDL_Log("Unable to initialize window: %s\n", SDL_GetError());
+        exit(EXIT_FAILURE);
+    }
+
 
     clearScreen();
     printf("Casse Briques\n");
@@ -39,6 +57,6 @@ int main(int argc, char **argv) {
 
     }
 
-
-    return 0;
+    SDL_Quit();
+    return EXIT_SUCCESS; //return 0;
 }
